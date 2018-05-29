fb42d46bc24a3fe1a85a4ea2fcea3b38a2fa8dcb
yangw <yang3.wang@intel.com>
2018-05-29 10:21:04 +0800

    test perf

---
+++ zoo/src/main/scala/com/intel/analytics/zoo/examples/tfnet/PerfTest.scala
@@ -0,0 +1,223 @@
+/*
+ * Copyright 2018 Analytics Zoo Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.intel.analytics.zoo.examples.tfnet
+
+import java.nio.file.Paths
+
+import com.intel.analytics.bigdl.{Module, TestModelBroadcast}
+import com.intel.analytics.bigdl.dataset.{MiniBatch, Sample, SampleToMiniBatch, Transformer}
+import com.intel.analytics.bigdl.models.utils.ModelBroadcast
+import com.intel.analytics.bigdl.nn.abstractnn.Activity
+import com.intel.analytics.bigdl.nn.{Contiguous, Sequential, Transpose}
+import com.intel.analytics.bigdl.optim.Predictor
+import com.intel.analytics.bigdl.optim.Predictor.{predictSamples, splitBatch}
+import com.intel.analytics.bigdl.tensor.Tensor
+import com.intel.analytics.bigdl.tensor.TensorNumericMath.TensorNumeric
+import com.intel.analytics.bigdl.tensor.TensorNumericMath.TensorNumeric.NumericFloat
+import com.intel.analytics.bigdl.transform.vision.image.{DistributedImageFrame, ImageFeature, ImageFrame}
+import com.intel.analytics.bigdl.utils.{T, Table}
+import com.intel.analytics.zoo.common.NNContext
+import com.intel.analytics.zoo.feature.image.{ImageMatToTensor, ImageResize, ImageSet, ImageSetToSample}
+import com.intel.analytics.zoo.pipeline.api.net.TFNet
+import org.apache.log4j.{Level, Logger}
+import scopt.OptionParser
+
+import scala.reflect.ClassTag
+
+
+object PerfTest {
+  Logger.getLogger("org").setLevel(Level.ERROR)
+  Logger.getLogger("akka").setLevel(Level.ERROR)
+  Logger.getLogger("breeze").setLevel(Level.ERROR)
+  Logger.getLogger("com.intel.analytics.zoo").setLevel(Level.INFO)
+
+  val logger = Logger.getLogger(getClass)
+
+  case class PredictParam(
+    image: String = "/tmp/datasets/cat_dog/train_sampled_test",
+    outputFolder: String = "data/demo",
+    model: String = "/home/yang/applications/faster_rcnn_resnet101_coco_2018_01_28" +
+      "/frozen_inference_graph.pb",
+    classNamePath: String = "/tmp/models/coco_classname.txt",
+    nPartition: Int = 1)
+
+  val parser = new OptionParser[PredictParam]("TFNet Object Detection Example") {
+    head("TFNet Object Detection Example")
+    opt[String]('i', "image")
+      .text("where you put the demo image data, can be image folder or image path")
+      .action((x, c) => c.copy(image = x))
+    opt[String]('o', "output")
+      .text("where you put the output data")
+      .action((x, c) => c.copy(outputFolder = x))
+    opt[String]('c', "classNamePath")
+      .text("where you put the class name file")
+      .action((x, c) => c.copy(outputFolder = x))
+    opt[String]("model")
+      .text("BigDL model")
+      .action((x, c) => c.copy(model = x))
+    opt[Int]('p', "partition")
+      .text("number of partitions")
+      .action((x, c) => c.copy(nPartition = x))
+  }
+
+  def main(args: Array[String]): Unit = {
+    parser.parse(args, PredictParam()).foreach { params =>
+
+      val sc = NNContext.getNNContext("TFNet Object Detection Example")
+
+      val inputs = Seq("ToFloat_3:0")
+      val outputs = Seq("num_detections:0", "detection_boxes:0",
+        "detection_scores:0", "detection_classes:0")
+
+      val detector = TFNet(params.model, inputs, outputs)
+      val model = Sequential()
+      model.add(Transpose(Array((2, 4), (2, 3))))
+      model.add(Contiguous())
+      model.add(detector)
+
+      val data = ImageSet.read(params.image, sc, minPartitions = params.nPartition)
+        .transform(ImageResize(256, 256) -> ImageMatToTensor() -> ImageSetToSample())
+
+
+      val output = predict(data.toImageFrame(), model)
+
+      // print the first result
+      val result = output.toDistributed().rdd.collect()
+      println(result)
+    }
+  }
+
+  private def predict(imageFrame: ImageFrame, model: Module[Float]) = {
+    val rdd = imageFrame.asInstanceOf[DistributedImageFrame].rdd
+    val modelBroad = TestModelBroadcast[Float]().broadcast(rdd.sparkContext, model.evaluate())
+    val partitionNum = rdd.partitions.length
+    val toBatchBroad = rdd.sparkContext.broadcast(SampleToMiniBatch(
+      batchSize = partitionNum * 1,
+      partitionNum = Some(partitionNum),
+      featurePaddingParam = None), false)
+    val result = rdd.mapPartitions(partition => {
+      val copyStart = System.nanoTime()
+      val localModel = modelBroad.value()
+      val copyEnd = System.nanoTime()
+      logger.info(s"Model Clone time: ${1.0 * (copyEnd - copyStart) / 1e6}ms")
+      val copyTransStart = System.nanoTime()
+      val localToBatch = toBatchBroad.value._1.cloneTransformer()
+      val copyTransEnd = System.nanoTime()
+      logger.info(s"Transformer Clone time: ${1.0 * (copyTransEnd - copyTransStart) / 1e6}ms")
+
+      val pipelineStart = System.nanoTime()
+      val re = partition.grouped(1).flatMap(imageFeatures => {
+        predictImageBatch[Float](localModel, imageFeatures, null, ImageFeature.predict,
+          localToBatch, false)
+      })
+      val arr = re.toArray
+      val pipelineEnd = System.nanoTime()
+      logger.info(s"pipeline calc time: ${1.0 * (pipelineEnd - pipelineStart) / 1e6}ms")
+      arr.toIterator
+    })
+    ImageFrame.rdd(result)
+  }
+
+  private def predictImageBatch[T: ClassTag](
+            localModel: Module[T], imageFeatures: Seq[ImageFeature],
+            outputLayer: String, predictKey: String,
+            localToBatch: Transformer[Sample[T], MiniBatch[T]],
+            shareBuffer: Boolean)(implicit ev: TensorNumeric[T]): Seq[ImageFeature] = {
+    val validImageFeatures = imageFeatures.filter(_.isValid)
+    val samples = validImageFeatures.map(x => x[Sample[T]](ImageFeature.sample))
+    val batchOut = predictSamples(localModel, samples, localToBatch, shareBuffer, outputLayer)
+    validImageFeatures.toIterator.zip(batchOut).foreach(tuple => {
+      tuple._1(predictKey) = tuple._2
+    })
+    imageFeatures
+  }
+
+  private def predictSamples[T: ClassTag]
+  (localModel: Module[T], samples: Seq[Sample[T]],
+   localToBatch: Transformer[Sample[T], MiniBatch[T]],
+   shareBuffer: Boolean,
+   outputLayer: String = null)(implicit ev: TensorNumeric[T]): Iterator[Activity] = {
+    val layer = if (outputLayer == null) {
+      localModel
+    } else {
+      val ol = localModel(outputLayer)
+      require(ol.isDefined, s"cannot find layer that map name $outputLayer")
+      ol.get
+    }
+    localToBatch(samples.toIterator).flatMap(batch => {
+      localModel.forward(batch.getInput())
+      splitBatch[T](layer.output, shareBuffer, batch.size())
+    })
+  }
+
+  private def splitBatch[T: ClassTag](output: Activity, shareBuffer: Boolean, batchSize: Int)
+                                            (implicit ev: TensorNumeric[T]): Array[Activity] = {
+    val out = if (output.isTensor) {
+      val result = if (shareBuffer) output.toTensor[T] else output.toTensor[T].clone()
+      if (result.dim() == 1) {
+        require(batchSize == 1,
+          s"If result dim == 1, the batchSize is required to be 1, while actual is $batchSize")
+        Array(result)
+      } else {
+        result.split(1)
+      }
+    } else {
+      val result = output.toTable
+      val first = result[Tensor[T]](1)
+      if (first.dim() == 1) {
+        require(batchSize == 1,
+          s"If result dim == 1, the batchSize is required to be 1, while actual is $batchSize")
+        val table = if (shareBuffer) {
+          result
+        } else {
+          val table = T()
+          (1 to result.length()).foreach(key => {
+            table.insert(result[Tensor[T]](key).clone())
+          })
+          table
+        }
+        Array(table)
+      } else {
+        val batch = first.size(1)
+        require(batch == batchSize, s"output batch $batch is not equal to input batch $batchSize")
+        val tables = new Array[Table](batch)
+        var i = 1
+        while (i <= batch) {
+          val table = T()
+          tables(i - 1) = table
+          (1 to result.length()).foreach(key => {
+            val split = result[Tensor[T]](key)(i)
+            if (shareBuffer) {
+              table.insert(split)
+            } else {
+              table.insert(split.clone())
+            }
+          })
+          i += 1
+        }
+        tables
+      }
+    }
+    out.asInstanceOf[Array[Activity]]
+  }
+
+  private def getOutPath(outPath: String, uri: String, encoding: String): String = {
+    Paths.get(outPath,
+      s"detection_${ uri.substring(uri.lastIndexOf("/") + 1,
+        uri.lastIndexOf(".")) }.${encoding}").toString
+  }
+}
---
+++ zoo/src/main/scala/com/intel/analytics/zoo/examples/tfnet/TestModelBroadcast.scala
@@ -0,0 +1,136 @@
+/*
+ * Copyright 2018 Analytics Zoo Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.intel.analytics.bigdl
+
+import com.intel.analytics.bigdl.Module
+import com.intel.analytics.bigdl.tensor.{QuantizedTensor, QuantizedType, Storage, Tensor}
+import com.intel.analytics.bigdl.tensor.TensorNumericMath.TensorNumeric
+import org.apache.spark.SparkContext
+import org.apache.spark.broadcast.Broadcast
+import com.intel.analytics.bigdl.utils.Util._
+
+
+import scala.reflect.ClassTag
+
+/**
+ * ModelBroadcast is used to broadcast model.
+ *
+ * Note: If you want to use this to broadcast training model, please use value(true) to get
+ * the model. And before broadcasting please make sure the model's parameter is compacted.
+ *
+ * @tparam T data type
+ * @param applyProtoBuffer it will use proto buffer serialization for broadcasting if set true
+ */
+class TestModelBroadcast[T: ClassTag](applyProtoBuffer: Boolean = false)
+                                 (implicit ev: TensorNumeric[T]) extends Serializable {
+
+  private var broadcastModel: Broadcast[Module[T]] = _
+  private var broadcastParameters: Broadcast[Array[Tensor[T]]] = _
+
+
+  /**
+   * broadcast the model
+   * first get and clear the weight and bias parameters from the model
+   * then broadcast the parameters and model(without parameters) separately
+   * @param sc    SparkContext
+   * @param model model to broadcast
+   * @return this
+   */
+  def broadcast(sc: SparkContext, model: Module[T]): this.type = {
+    if (applyProtoBuffer) {
+      broadcastModel = sc.broadcast(model)
+    } else {
+      val weightsBias = getAndClearWeightBias(model.parameters())
+      broadcastModel = sc.broadcast(model.cloneModule())
+      broadcastParameters = sc.broadcast(weightsBias)
+      putWeightBias(weightsBias, model)
+      initGradWeightBias(weightsBias, model)
+    }
+    this
+  }
+
+  /**
+   * get the broadcast model
+   * put the weight and bias back to the model
+   *
+   * @param initGradient if init gradParameter.
+   * @return model
+   */
+  def value(initGradient: Boolean = false): Module[T] = {
+    if (applyProtoBuffer) {
+      val localModel = broadcastModel.value.clone(false)
+      if (initGradient) {
+        initGradWeightBias(getWeightBias(localModel.parameters()), localModel)
+      }
+      localModel
+    } else {
+      val localModel = broadcastModel.value.cloneModule()
+      putWeightBias(broadcastParameters.value, localModel)
+      if (initGradient) {
+        initGradWeightBias(broadcastParameters.value, localModel)
+      }
+      localModel
+    }
+  }
+
+  private def getWeightBias(parameters: (Array[Tensor[T]], Array[Tensor[T]]))
+  : Array[Tensor[T]] = {
+    if (parameters._1.length != 0) {
+      var i = 0
+      val weightsBias = new Array[Tensor[T]](parameters._1.length)
+      val isQuantized = parameters._1.exists(_.getTensorType == QuantizedType)
+      val (isCompacted, storage) = if (!isQuantized) {
+        val storage = Storage(parameters._1(0).storage.array())
+        (parameters._1.map(_.nElement()).sum == storage.length(), storage)
+      } else {
+        (false, null)
+      }
+
+      // get weight and bias
+      while (i < parameters._1.length) {
+        if (parameters._1(i) != null) {
+          val wb = parameters._1(i)
+          wb.getTensorType match {
+            case QuantizedType =>
+              val quantTensor = wb.asInstanceOf[QuantizedTensor[T]]
+              weightsBias(i) = QuantizedTensor[T](quantTensor.getStorage, quantTensor.maxOfRow,
+                quantTensor.minOfRow, quantTensor.sumOfRow, quantTensor.size(), quantTensor.params)
+            case _ =>
+              weightsBias(i) = if (isCompacted) {
+                Tensor[T](storage, wb.storageOffset(), wb.size(), wb.stride())
+              } else {
+                Tensor[T](Storage(wb.storage().array()), wb.storageOffset(), wb.size(), wb.stride())
+              }
+          }
+          i += 1
+        }
+      }
+      weightsBias
+    } else {
+      // just return an empty array when parameters is empty.
+      Array()
+    }
+  }
+
+}
+
+
+object TestModelBroadcast {
+  def apply[@specialized(Float, Double) T: ClassTag](applyProtoBuffer: Boolean = false)
+         (implicit ev: TensorNumeric[T]) : TestModelBroadcast[T] = {
+    new TestModelBroadcast(applyProtoBuffer)
+  }
+}
